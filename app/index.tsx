a6c7eae6cc5c66ecc051ceb6fbd7bf9ed3eb263e
maphy9 <maphy9@gmail.com>
2025-10-05 13:29:02 +0200

    chore: Add expo router

---
+++ app/index.tsx
@@ -0,0 +1,21 @@
+import { View, Text } from "react-native";
+import { StyleSheet } from "react-native";
+import React from "react";
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
