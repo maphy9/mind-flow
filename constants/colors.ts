57cff04f1022b27a9a933c9163a05354f5cfa554
maphy9 <maphy9@gmail.com>
2025-10-12 01:23:23 +0200

    chore: Change color theme

--- constants/colors.ts
+++ constants/colors.ts
@@ -1,20 +1,20 @@
 export const Colors = {
   dark: {
-    primary: "#0D1A11",
+    primary: "#444136",
     primaryAccent: "#232C26",
     secondary: "#F5F5F4",
-    secondaryAccent: "#A8A29E",
-    surface: "#19E65E",
-    surfaceAccent: "#0F4220",
-    red: "#9E2E35",
+    secondaryAccent: "#B7B0AA",
+    surface: "#2B5743",
+    surfaceAccent: "#4F876D",
+    red: "#954247",
   },
   light: {
     primary: "#FAF9F6",
     primaryAccent: "#E5E7EB",
     secondary: "#212121",
     secondaryAccent: "#757575",
-    surface: "#4CAF50",
+    surface: "#57B88A",
     surfaceAccent: "#D5EDD7",
-    red: "#9E2E35",
+    red: "#954247",
   },
 };
