ea93282c4080ed5809fb3e3053e7513959af2998
maphy9 <maphy9@gmail.com>
2025-11-04 18:13:17 +0100

    Fix push notification time policy

--- utils/notifications.ts
+++ utils/notifications.ts
@@ -45,9 +45,9 @@ export async function scheduleDailyNotification() {
       sound: true,
     },
     trigger: {
-      type: Notifications.SchedulableTriggerInputTypes.TIME_INTERVAL,
-      seconds: 15,
-      repeats: true,
+      type: Notifications.SchedulableTriggerInputTypes.DAILY,
+      hour: 12,
+      minute: 30,
     },
   });
 }
